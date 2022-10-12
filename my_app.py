b548b60cb958f2e551e7c0935b2fc3e39fcb839c
JhuN <ceyhun_yildiz@hotmail.com>
2022-10-12 12:34:11 +0300

    first commit

---
+++ my_app.py
@@ -0,0 +1,21 @@
+from cmath import pi
+
+
+name=input('What is your name ? ')
+print('Hello '+name)
+
+name2='bade'
+surname='Yıldız'
+age=8
+pi=3.14
+cars = ['bmw','mercedes','range rover']
+print(name2)
+print(age)
+print(pi)
+print(cars)
+fullName=name2.capitalize()+' '+surname
+print(fullName)
+
+print(len(name2))
+print(len(surname))
+print(fullName.endswith('hello'))
